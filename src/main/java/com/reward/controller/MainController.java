b0c403393446a77805d4c1082409a77aa199ddb2
gejiaxing <hzgjx@trairanchina.com>
2017-02-03 15:21:42 +0800

    add logback

--- src/main/java/com/reward/controller/MainController.java
+++ src/main/java/com/reward/controller/MainController.java
@@ -3,6 +3,8 @@ package com.reward.controller;
 import com.reward.dao.RewardShareDao;
 import com.reward.model.RewardShareDo;
 import com.reward.properties.GirlProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 public class MainController {
+    Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @Value("${shareId}")
     private String shareId;
@@ -27,6 +30,7 @@ public class MainController {
         RewardShareDo rewardShareDo = new RewardShareDo();
         rewardShareDo.setId(shareId);
         rewardShareDo = rewardShareDao.getModel(rewardShareDo);
+        logger.info("获取成功{}",rewardShareDo.getCount());
         return rewardShareDo.getSceneId();
     }
 
--- src/test/java/com/reward/GirlApplicationTests.java
+++ src/test/java/com/reward/GirlApplicationTests.java
@@ -1,16 +1,26 @@
 package com.reward;
 
+import com.reward.dao.RewardShareDao;
+import com.reward.model.RewardShareDo;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GirlApplicationTests {
+    @Autowired
+    private RewardShareDao rewardShareDao;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void getModel() {
+        RewardShareDo rewardShareDo = new RewardShareDo();
+        rewardShareDo.setId("0D1906BB641748B782E227FAE077FF6");
+        rewardShareDo = rewardShareDao.getModel(rewardShareDo);
+        Assert.assertNotNull("为空", rewardShareDo.getSceneId());
+    }
 
 }
