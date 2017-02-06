778f8997651f186a268ce21b5afc49d9caa5363e
gejiaxing <hzgjx@trairanchina.com>
2017-02-06 09:43:48 +0800

    add redis config setting

--- src/main/java/com/reward/controller/MainController.java
+++ src/main/java/com/reward/controller/MainController.java
@@ -7,7 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 /**
  * Created by gjason on 2017/1/27.
@@ -25,12 +29,16 @@ public class MainController {
     @Autowired
     private RewardShareDao rewardShareDao;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @RequestMapping(value = {"/", "index"}, method = RequestMethod.GET)
     public String index() {
         RewardShareDo rewardShareDo = new RewardShareDo();
         rewardShareDo.setId(shareId);
         rewardShareDo = rewardShareDao.getModel(rewardShareDo);
         logger.info("获取成功{}",rewardShareDo.getCount());
+        redisTemplate.opsForValue().set("sceneId",rewardShareDo.getSceneId());
         return rewardShareDo.getSceneId();
     }
 
